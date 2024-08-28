e5c2390bda85c7eed33436b75e1e5f0f0f4851b8
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2024-08-27 20:15:34 -0400

    adding rusty audio implementation for waveforms. miniaudio dependency is hidden via pimpl idiom

--- include/rusty_audio.h
+++ include/rusty_audio.h
@@ -1,8 +1,31 @@
 #pragma once
 
+#include <memory>
+
 namespace RustyAudio
 {
 
+struct Note
+{
+    int milliseconds;
+    double amplitude, frecuency;
+};
 
+class SoundManager
+{
+public:
+    SoundManager();
+
+    void scheduleNote(const Note& note);
+
+    void playNext();
+
+    void tick(float milliseconds);
+
+    ~SoundManager();
+private:
+    class SoundManagerImpl;
+    std::unique_ptr<SoundManagerImpl> mSoundManagerImpl;
+};
 
 }
